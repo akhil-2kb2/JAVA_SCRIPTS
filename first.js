e761cad9aa4ad52b88ff7cb357567df082fcb84c
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-13 23:38:54 +0530

    adding first

---
+++ first.js
@@ -0,0 +1,50 @@
+let Name = "Akhilesh"; // String
+//let Name = "Akhilesh"; //  Error: Identifier 'Name' has already been declared
+const age = 25; // const variable can't be re-declared or updated without a value
+//age = 26; // Error: Assignment to constant variable/
+// var isLearning = true; // Boolean
+// var isLearning = false; //var can be re-declared and updated
+
+// console.log(Name, age, isLearning);
+
+// console.log(typeof "Hello"); // String
+// let num = BigInt("42");
+// console.log(typeof BigInt("42")); // BigInt
+// console.log(typeof true); // Boolean
+// console.log(typeof undefined); // Undefined
+// console.log(typeof null); // Object (special case)
+// console.log(typeof { name: "Akhilesh" }); // Object
+// console.log(typeof Symbol("id")); // Symbol
+
+
+// const student = {
+//     fullName : "Akhilesh Yadav",
+//     age : 25,
+//     isLearning : true,
+//     CGPA : 8.5,
+// };
+// console.log(student.fullName);
+// console.log(student ["age"]);
+// student["age"] = student["age"] + 1;
+// console.log(student.age);
+// console.log(student.isLearning);
+// console.log(student["CGPA"]);
+
+// const product = {
+//     title : "Ball Pen",
+//     rating : 4.5,
+//     price : 270,
+//     offer : 5,
+// };
+// console.log(product);
+const profile = {
+    username : "@ak2kb2Akhilesh",
+    isFollow : true,
+    followers : 1000,
+    following : 500,
+    posts : 100,
+    comments : 500,
+    likes : 10000,
+    bio : "I am a web developer",
+};
+    console.log(profile);
